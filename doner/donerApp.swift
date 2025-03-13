ed03dec79e17b9e3d58ee24a9af581a84974e912
Hanna Skairipa <hanna@hannaskairipa.com>
2025-03-13 16:05:55 -0600

    Initial Commit

---
+++ doner/donerApp.swift
@@ -0,0 +1,17 @@
+//
+//  donerApp.swift
+//  doner
+//
+//  Created by Hanna Skairipa on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct donerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ donerTests/donerTests.swift
@@ -0,0 +1,17 @@
+//
+//  donerTests.swift
+//  donerTests
+//
+//  Created by Hanna Skairipa on 3/13/25.
+//
+
+import Testing
+@testable import doner
+
+struct donerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
